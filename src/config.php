3ea84eba0a090ad3940828be1c747f2eeb826ddb
Jan Henckens <jan@studioespresso.co>
2018-05-06 12:48:22 +0200

    Added example config

--- src/config.php
+++ src/config.php
@@ -8,23 +8,17 @@
  * @copyright Copyright (c) 2018 Studio Espresso
  */
 
-/**
- * Seeder config.php
- *
- * This file exists only as a template for the Seeder settings.
- * It does nothing on its own.
- *
- * Don't edit this file, instead copy it to 'craft/config' as 'seeder.php'
- * and make your changes there to override default settings.
- *
- * Once copied to 'craft/config', this file will be multi-environment aware as
- * well, so you can have different settings groups for each environment, just as
- * you do for 'general.php'
- */
-
 return [
-
-    // This controls blah blah blah
-    "someAttribute" => true,
-
+    "sets" => [
+        'default' => [
+            'Users' => [
+                // groupId => count
+                1 => 10
+            ],
+            "Entries" => [
+                // sectionId => count
+                1 => 20,
+            ]
+        ],
+    ],
 ];
