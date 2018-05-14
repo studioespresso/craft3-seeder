834e7478208a20d513a04667f0ccb0e3f39f0a82
Jan Henckens <jan@studioespresso.co>
2018-05-14 19:17:18 +0200

    Save the entrys uid instead of the id

--- src/migrations/Install.php
+++ src/migrations/Install.php
@@ -14,52 +14,49 @@ use yii\db\Schema;
  */
 class Install extends Migration
 {
-	/**
-	 * @inheritdoc
-	 */
-	public function safeUp()
-	{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
 
-		$this->createTable(
-			SeederEntryRecord::$tableName, [
-			'id' => $this->primaryKey(),
-			'entryId' => $this->integer()->notNull(),
-			'section' => $this->integer()->notNull(),
+        $this->createTable(
+            SeederEntryRecord::$tableName, [
+            'id' => $this->primaryKey(),
+            'entryUid' => $this->uid()->notNull(),
+            'section' => $this->integer()->notNull(),
+            'dateCreated' => $this->dateTime()->notNull(),
+            'dateUpdated' => $this->dateTime()->notNull(),
+            'uid' => $this->uid()->notNull(),
+        ]);
 
-			'dateCreated' => $this->dateTime()->notNull(),
-			'dateUpdated' => $this->dateTime()->notNull(),
-			'uid'         => $this->uid()->notNull(),
-		] );
-
-		$this->createTable(
-			SeederAssetRecord::$tableName, [
-			'id' => $this->primaryKey(),
-			'assetId' => $this->integer()->notNull(),
-
-			'dateCreated' => $this->dateTime()->notNull(),
-			'dateUpdated' => $this->dateTime()->notNull(),
-			'uid'         => $this->uid()->notNull(),
-		] );
+        $this->createTable(
+            SeederAssetRecord::$tableName, [
+            'id' => $this->primaryKey(),
+            'assetUid' => $this->uid()->notNull(),
+            'dateCreated' => $this->dateTime()->notNull(),
+            'dateUpdated' => $this->dateTime()->notNull(),
+            'uid' => $this->uid()->notNull(),
+        ]);
 
         $this->createTable(
             SeederUserRecord::$tableName, [
             'id' => $this->primaryKey(),
-            'userId' => $this->integer()->notNull(),
-
+            'userUid' => $this->uid()->notNull(),
             'dateCreated' => $this->dateTime()->notNull(),
             'dateUpdated' => $this->dateTime()->notNull(),
-            'uid'         => $this->uid()->notNull(),
-        ] );
+            'uid' => $this->uid()->notNull(),
+        ]);
 
-	}
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public function safeDown()
-	{
-		$this->dropTable(SeederEntryRecord::$tableName);
-		$this->dropTable(SeederAssetRecord::$tableName);
-		$this->dropTable(SeederUserRecord::$tableName);
-	}
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        $this->dropTable(SeederEntryRecord::$tableName);
+        $this->dropTable(SeederAssetRecord::$tableName);
+        $this->dropTable(SeederUserRecord::$tableName);
+    }
 }
