d78545514db68e563d1069a234178402c31a4222
Jan Henckens <jan@studioespresso.co>
2018-06-01 19:46:34 +0200

    Bootstrapped codeception classes

---
+++ tests/unit/FieldTest.php
@@ -0,0 +1,15 @@
+<?php
+
+class FieldTest extends \Codeception\Test\Unit {
+	/**
+	 * @var \UnitTester
+	 */
+	protected $tester;
+
+	protected function _before() {
+	}
+
+	protected function _after() {
+	}
+
+}
